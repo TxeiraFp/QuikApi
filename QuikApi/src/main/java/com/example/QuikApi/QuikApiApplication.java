48204ee05e445ed818862b43abd0f1c26ca7a712
TxeiraFp <joao.felpets400@gmail.com>
2025-01-08 12:55:55 -0400

    the one commit

---
+++ QuikApi/src/main/java/com/example/QuikApi/QuikApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuikApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuikApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuikApiApplication.class, args);
+	}
+
+}
---
+++ QuikApi/src/test/java/com/example/QuikApi/QuikApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuikApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuikApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
